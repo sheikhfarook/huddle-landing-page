d6aeb7c0e4acde5aea6d94c9fbc3a92429752bcc
Farook <mohammedfarook085@gmail.com>
2023-12-04 11:20:15 +0530

    import files

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import msgIcon from "../";
+
+const HeaderIcon = () => {
+  return (
+    <div>
+      <img src={msgIcon} alt="" />
+    </div>
+  );
+};
+
+export default HeaderIcon;
